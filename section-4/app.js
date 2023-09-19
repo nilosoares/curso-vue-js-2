40eea28803a41e22a1e56618d651924ddb675b47
Nilo Soares <nilosoares@hotmail.com>
2023-09-19 07:01:54 -0300

    Show and hide buttons according to the state

--- section-4/app.js
+++ section-4/app.js
@@ -1,4 +1,23 @@
+const STATE = {
+    MENU: 0,
+    PLAYING: 1,
+    ENDED: 2,
+};
+
 new Vue({
     el: '#app',
-   
+    data: {
+        state: STATE.MENU,
+    },
+    computed: {
+        showMenu() {
+            return this.state === STATE.MENU;
+        },
+        showActions() {
+            return this.state === STATE.PLAYING;
+        },
+        showResult() {
+            return this.state === STATE.ENDED;
+        }
+    },
 })
