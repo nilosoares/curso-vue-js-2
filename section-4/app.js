37f6ab8cb0d3b9b9a3db6fe58f69f4dcf2170c78
Nilo Soares <nilosoares@hotmail.com>
2023-09-19 07:24:13 -0300

    Add methods to start the game, attack, heal and give up...

--- section-4/app.js
+++ section-4/app.js
@@ -4,20 +4,88 @@ const STATE = {
     ENDED: 2,
 };
 
+const MATCH_RESULT = {
+    UNKNOWN: 0,
+    WIN: 1,
+    LOST: 2,
+};
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
 new Vue({
     el: '#app',
     data: {
         state: STATE.MENU,
+        matchResult: MATCH_RESULT.UNKNOWN,
+        playerLife: 100,
+        monsterLife: 100,
     },
     computed: {
         showMenu() {
-            return this.state === STATE.MENU;
+            return this.state === STATE.MENU || this.state === STATE.ENDED;
         },
         showActions() {
             return this.state === STATE.PLAYING;
         },
         showResult() {
             return this.state === STATE.ENDED;
+        },
+        youWin() {
+            return this.playerLife > 0;
+        },
+        youLost() {
+            return this.playerLife <= 0;
+        },
+    },
+    methods: {
+        start() {
+            this.state = STATE.PLAYING;
+            this.playerLife = 100;
+            this.monsterLife = 100;
+        },
+        attack() {
+            const attack = getRandomInt(5, 12);
+            this.monsterLife -= attack;
+            this.monsterAttack();
+        },
+        specialAttack() {
+            const attack = getRandomInt(13, 20);
+            this.monsterLife -= attack;
+            this.monsterAttack();
+        },
+        heal() {
+            const heal = getRandomInt(11, 18);
+            this.playerLife += Math.max(100, heal);
+            this.monsterAttack();
+        },
+        monsterAttack() {
+            const attack = getRandomInt(10, 15);
+            this.playerLife -= attack;
+        },
+        giveUp() {
+            this.state = STATE.MENU;
+        },
+    },
+    watch: {
+        playerLife(value) {
+            if (value < 0) {
+                this.state = STATE.ENDED;
+            } else if (value > 100) {
+                this.playerLife = 100;
+            } else {
+                this.playerLife = value;
+            }
+        },
+        monsterLife(value) {
+            if (value < 0) {
+                this.state = STATE.ENDED;
+            } else if (value > 100) {
+                this.monsterLife = 100;
+            } else {
+                this.monsterLife = value;
+            }
         }
     },
 })
